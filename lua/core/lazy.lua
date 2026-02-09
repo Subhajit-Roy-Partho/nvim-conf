715de6bdca3df923e6b5f5f6cae854b153be8a95
Subhajit-Roy-Partho <me.subhajitroy1999@gmail.com>
2026-02-09 03:49:47 -0700

    Create minimal fast profile for reading, writing, and navigation

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -1,5 +1,4 @@
 -- [[ Install `lazy.nvim` plugin manager ]]
---    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
@@ -7,32 +6,13 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     error('Error cloning lazy.nvim:\n' .. out)
   end
-end ---@diagnostic disable-next-line: undefined-field
+end
 vim.opt.rtp:prepend(lazypath)
 
--- Setup lazy.nvim
 require('lazy').setup({
-  -- Import all plugin modules
   { import = 'plugins' },
-  { import = 'kickstart.plugins' },
-
-  ui = {
-    -- If you are using a Nerd Font: set icons to an empty table which will use the
-    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-    icons = vim.g.have_nerd_font and {} or {
-      cmd = '⌘',
-      config = '🛠',
-      event = '📅',
-      ft = '📂',
-      init = '⚙',
-      keys = '🗝',
-      plugin = '🔌',
-      runtime = '💻',
-      require = '🌙',
-      source = '📄',
-      start = '🚀',
-      task = '📌',
-      lazy = '💤 ',
-    },
-  },
+  { import = 'custom.plugins' },
+}, {
+  checker = { enabled = false },
+  change_detection = { enabled = false },
 })
--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,180 +1,21 @@
--- Editor enhancement plugins
+-- Minimal editor plugins for writing and reading
 
 return {
-  -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
-
-  -- Git signs
-  { -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    event = { 'BufReadPre', 'BufNewFile' },
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
-  },
-
-  -- Todo comments
   {
-    'folke/todo-comments.nvim',
-    event = 'VeryLazy',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    },
-  },
-
-  -- Toggle terminal
-  {
-    'akinsho/toggleterm.nvim',
-    cmd = { 'ToggleTerm', 'TermExec' },
-    keys = {
-      { '<C-`>', '<cmd>ToggleTerm<CR>', desc = 'Toggle terminal' },
-    },
-    config = function()
-      require('toggleterm').setup {
-        size = 10, -- Default terminal height
-        direction = 'horizontal', -- Make the terminal horizontal
-      }
-    end,
-  },
-
-  -- Treesitter
-  { -- Highlight, edit, and navigate code
-    'nvim-treesitter/nvim-treesitter',
-    event = { 'BufReadPost', 'BufNewFile' },
-    branch = 'master',
-    build = ':TSUpdate',
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-    opts = {
-      ensure_installed = {
-        'bash',
-        'c',
-        'diff',
-        'html',
-        'lua',
-        'luadoc',
-        'markdown',
-        'markdown_inline',
-        'query',
-        'vim',
-        'vimdoc',
-      },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        -- If you are experiencing weird indenting issues, add the language to
-        -- the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-    },
-    -- There are additional nvim-treesitter modules that you can use to interact
-    -- with nvim-treesitter. You should go explore a few and see what interests you:
-    --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-  },
-  -- Auto tag
-  {
-    'windwp/nvim-ts-autotag',
-    ft = { 'html', 'xml' },
-    config = function()
-      require('nvim-ts-autotag').setup {
-        opts = {
-          -- Defaults
-          enable_close = true, -- Auto close tags
-          enable_rename = true, -- Auto rename pairs of tags
-          enable_close_on_slash = false, -- Auto close on trailing </
-        },
-        -- Also override individual filetype configs, these take priority.
-        -- Empty by default, useful if one of the "opts" global settings
-        -- doesn't work well in a specific filetype
-        per_filetype = {
-          ['html'] = {
-            enable_close = false,
-          },
-        },
-      }
-    end,
-  },
-
-  -- Mini.nvim collection
-  { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
     event = 'VeryLazy',
     config = function()
-      -- Better Around/Inside textobjects
-      --
-      -- Examples:
-      --  - va)  - [V]isually select [A]round [)]paren
-      --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-      --  - ci'  - [C]hange [I]nside [']quote
-      require('mini.ai').setup { n_lines = 500 }
-
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
+      require('mini.ai').setup { n_lines = 300 }
       require('mini.surround').setup()
-
-      -- ... and there is more!
-      --  Check out: https://github.com/echasnovski/mini.nvim
     end,
   },
-
-  -- Visual multi cursor
-  {
-    'mg979/vim-visual-multi',
-    event = 'VeryLazy', -- This loads the plugin lazily
-    config = function()
-      -- You can add any plugin-specific configuration here if needed.
-      -- For example, setting up custom mappings (optional).
-      -- The default configuration works well out of the box.
-    end,
-  },
-
-  -- Bullets for markdown
   {
     'bullets-vim/bullets.vim',
     ft = { 'markdown', 'text' },
     config = function()
-      -- Optional configuration if you want to adjust bullet style or behavior
       vim.g.bullets_enabled_file_types = { 'markdown', 'text' }
-      -- For example, to disable extra padding:
-      -- vim.g.bullets_pad_right = 0
     end,
   },
-
-  -- Wrap width
-  { 'rickhowe/wrapwidth', ft = { 'markdown', 'text' }, config = function() end },
-
-  -- Better indent guides
-  {
-    'lukas-reineke/indent-blankline.nvim',
-    event = { 'BufReadPost', 'BufNewFile' },
-    main = 'ibl',
-    config = function()
-      require('ibl').setup {
-        indent = {
-          char = '┊',
-        },
-        scope = {
-          enabled = true,
-          show_start = true,
-        },
-      }
-    end,
-  },
+  { 'rickhowe/wrapwidth', ft = { 'markdown', 'text' } },
 }
